4daa5a5a684644fac7d86100b5952fab1f896e46
Tiago Veiga Lázaro <tiago@unravelling.io>
2021-10-14 15:47:49 +0200

    Added base hello world example.

---
+++ src/test/java/io/unravelling/demos/springboot/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.unravelling.demos.springboot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
